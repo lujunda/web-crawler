bdbccaf1263bf6ed4d84043e44fc997deaf770aa
lujunda <lujunda@cmcm.com>
2019-05-05 17:10:02 +0800

    update

--- main.go
+++ main.go
@@ -2,15 +2,15 @@ package main
 
 import (
 	"fmt"
+	_ "io"
 	"io/ioutil"
-	_"io"
-	_"os"
-	"os/exec"
-	"time"
-	"sync"
 	"net/http"
+	_ "os"
+	"os/exec"
 	"regexp"
 	"strings"
+	"sync"
+	"time"
 )
 
 type Web struct {
@@ -22,18 +22,23 @@ type Web struct {
 
 var ch chan string
 var wg sync.WaitGroup
+var lock sync.RWMutex
 
 func dfs(url string, m map[string]bool) {
+	lock.RLock()
 	_, ok := m["https://www.ishsh.com"+url]
+	lock.RUnlock()
 	if ok {
-        fmt.Println(<-ch)
-        wg.Done()
+		fmt.Println(<-ch)
+		wg.Done()
 		return
 	} else {
+		lock.Lock()
 		m["https://www.ishsh.com"+url] = true
+		lock.Unlock()
 	}
 
-	resp, _ := http.Get("https://www.ishsh.com"+url)
+	resp, _ := http.Get("https://www.ishsh.com" + url)
 	defer resp.Body.Close()
 
 	body, _ := ioutil.ReadAll(resp.Body)
@@ -58,53 +63,51 @@ func dfs(url string, m map[string]bool) {
 
 		reg_fmt := regexp.MustCompile(`[\.]{1}([a-zA-Z]+?)$`)
 		file_fmt := reg_fmt.FindStringSubmatch(pics[3])
-        if len(file_fmt) > 0 {
-            file_name = file_name + file_fmt[0]
-        }
+		if len(file_fmt) > 0 {
+			file_name = file_name + file_fmt[0]
+		}
 
-
-        file_name = strings.Replace(file_name, " ", "_", -1)
-        dir_name = strings.Replace(dir_name, " ", "_", -1)
+		file_name = strings.Replace(file_name, " ", "_", -1)
+		dir_name = strings.Replace(dir_name, " ", "_", -1)
 		fmt.Println(file_name)
 
-        full_cmd := "mkdir -p /data/file/" + dir_name + " && wget " + pics[3] + " -O " + "/data/file/" + dir_name + "/" + file_name + " -P " + "/data/file/" + dir_name
+		full_cmd := "mkdir -p /data/file/" + dir_name + " && wget " + pics[3] + " -O " + "/data/file/" + dir_name + "/" + file_name + " -P " + "/data/file/" + dir_name
 
 		fmt.Println(full_cmd)
 		out, err := exec.Command("/bin/bash", "-c", full_cmd).Output()
-        fmt.Println(err)
-        fmt.Println(out)
+		fmt.Println(err)
+		fmt.Println(out)
 
-
-        /*
-        pic, _ := http.Get(pics[3])
-        file, err := os.Create(full_name)
-        fmt.Println(err)
-        io.Copy(file, pic.Body)
-        */
-
+		/*
+		   pic, _ := http.Get(pics[3])
+		   file, err := os.Create(full_name)
+		   fmt.Println(err)
+		   io.Copy(file, pic.Body)
+		*/
 
 	}
 
-    fmt.Println(<-ch)
+	fmt.Println(<-ch)
 
 	reg := regexp.MustCompile(`/([\d]+)([\_]*)([\d]*).html`)
 	nexts := reg.FindAllString(html, -1)
 	for _, v := range nexts {
-        ch <- v
-        wg.Add(1)
+		ch <- v
+		wg.Add(1)
 		go dfs(v, m)
 	}
-    wg.Done()
+	wg.Done()
 }
 
 func main() {
-    ch = make(chan string, 8)
+	ch = make(chan string, 40)
 	m := make(map[string]bool)
+	lock = sync.RWMutex{}
 
-    ch <- "root"
-    wg.Add(1)
+	ch <- "root"
+	wg.Add(1)
 	go dfs("/", m)
 
-    time.Sleep(time.Second * 5)
-    wg.Wait()
+	time.Sleep(time.Second * 5)
+	wg.Wait()
 }
